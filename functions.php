1e434f68aeb0b3c8602f74ea52562f7c8eaa160f
Kristijan Soldo <soldokris@gmail.com>
2025-06-04 10:06:08 +0200

    local terminal.min.css

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function terminal_theme_enqueue_styles() {
-    wp_enqueue_style('terminal-css', 'https://unpkg.com/terminal.css@0.7.4/dist/terminal.min.css', array(), '0.7.4');
+    wp_enqueue_style('terminal-css', get_stylesheet_directory_uri() . '/css/terminal.min.css', array(), '0.7.4');
     wp_enqueue_style('console-css', get_stylesheet_directory_uri() . '/css/console.css', array('terminal-css'), '1.0.0');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array('terminal-css'), '1.0.0');
 }
